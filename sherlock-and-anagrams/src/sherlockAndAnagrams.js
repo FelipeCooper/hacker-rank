bb30c7547151d72561e1a76267d0191d7b82bbb9
Felipe Cooper <marcos.cooper@softplan.com.br>
2022-06-21 22:23:49 -0300

    sherlock and anagramas: removed isAnagram because is very slow

--- sherlock-and-anagrams/src/sherlockAndAnagrams.js
+++ sherlock-and-anagrams/src/sherlockAndAnagrams.js
@@ -1,27 +1,19 @@
 function sherlockAndAnagrams(text) {
   let arr = [];
   let count = 0;
-  for (let i = 0; i < text.length + 1; i++) {
+  for (let i = 0; i < text.length; i++) {
     for (let j = i + 1; j < text.length + 1; j++) {
-      let substring = text.substring(i, j);
-      if (substring === text) continue;
-      arr.push(substring);
+      arr.push(text.substring(i, j).split("").sort().join(""));
     }
   }
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      if (i !== j && isAnagrams(arr[i], arr[j])) {
+      if (arr[i] === arr[j]) {
         count++;
       }
     }
   }
   return count;
-}
-
-function isAnagrams(string1, string2) {
-  return (
-    string1.split("").sort().join("") === string2.split("").sort().join("")
-  );
 }
 
 module.exports = sherlockAndAnagrams;
